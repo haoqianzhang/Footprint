51bb949a1947875acb9860c6c66604c9f05f7d7b
yohoo <qtcdyx@126.com>
2015-11-22 06:17:36 +0800

    go to sleep!

---
+++ mapChoiceSample/VLDContextSheetExample/VLDContextSheetExample/VLDAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  VLDAppDelegate.h
+//  VLDContextSheetExample
+//
+//  Created by Vladimir Angelov on 11/2/14.
+//  Copyright (c) 2014 Vladimir Angelov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class VLDExampleViewController;
+
+@interface VLDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) VLDExampleViewController *viewController;
+
+@end
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com 
